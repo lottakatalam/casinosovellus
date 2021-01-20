4adf3ca47b41fccb8ce096d1d5c0b00520b3d56a
lottakatala <lampola.lotta@gmail.com>
2021-01-20 16:44:03 +0200

    Made an enum class for suits and the first version of the card-class

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -1,4 +1,19 @@
 package model;
 
 public class Card {
+    private int value;
+    private Suit suit;
+
+    public Card(int value, Suit suit) {
+       this.value = value;
+       this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
 }
