9223a78c2672ec9bc592caed627cbaf9c7eeebd2
lottakatala <lampola.lotta@gmail.com>
2021-01-21 13:14:03 +0200

    Added first version of Hand-class

--- src/main/java/model/Hand.java
+++ src/main/java/model/Hand.java
@@ -1,6 +1,19 @@
 package model;
 
 public class Hand {
+    /**
+     * Integer which represents the maximum amount of cards in hand.
+     * Created so the concept of hand-object can be used in other card games as well.
+     */
+    private int maxCards;
+    /**
+     * An array of Card-objects.
+     */
+    private Card[] hand;
 
+    public Hand(int maxCards) {
+        this.maxCards = maxCards;
+        this.hand = new Card[this.maxCards];
+    }
 
 }
