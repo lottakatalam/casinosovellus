1b1776ea1e1f42817c337ac9c22b553c0f368ac1
teemuluh <teemu.luhtanen@metropolia.fi>
2021-01-23 18:58:09 +0200

    Testing

---
+++ src/main/java/fxml.java
@@ -0,0 +1,23 @@
+import com.sun.javafx.iio.ImageLoader;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class fxml extends Application{
+    @Override
+    public void start(Stage primaryStage) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("InGameView.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
