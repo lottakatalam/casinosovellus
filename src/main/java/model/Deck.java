b82dc0d3d91b553b648da2038a77bcf264aa8aef
lottakatala <lampola.lotta@gmail.com>
2021-01-24 12:38:16 +0200

    Revert "Changed the data structure of Deck-class from Array to ArrayList"
    This reverts commit b2b8cbe3

--- src/main/java/model/Deck.java
+++ src/main/java/model/Deck.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.ArrayList;
-
 /**
  * Represents a deck of cards
  *
@@ -14,7 +12,7 @@ public class Deck {
      * An array of cards which represents the deck.
      * Can contain maximum of 52 objects.
      */
-    private ArrayList<Card> deck;
+    private Card[] deck = new Card[52];
 
     /**
      * Initializes a new Deck-object and creates 52 Card-objects for the deck.
@@ -22,9 +20,8 @@ public class Deck {
     public Deck() {
         int rank = 1;
         int suit = 1;
-        this.deck = new ArrayList<>();
         for (int i = 0; i < 52; i++) {
-            this.deck.add(new Card(rank, suit));
+            this.deck[i] = new Card(rank, suit);
             rank++;
             if (rank > 13) {
                 rank = 1;
@@ -37,17 +34,8 @@ public class Deck {
      * Returns cards in deck
      * @return array of cards
      */
-
-    public ArrayList<Card> getCardsInDeck() {
+    public Card[] getCardsInDeck() {
         return deck;
-    }
-
-    /**
-     * Returns the first Card-object and removes it from the ArrayList
-     * @return the first card of the deck-array
-     */
-    public Card nextCard(){
-        return this.deck.remove(0);
     }
 
     /**
@@ -55,11 +43,11 @@ public class Deck {
      *
      */
     public void shuffleDeck() {
-        for (int i=0;i<deck.size();i++) {
+        for (int i=0;i<deck.length;i++) {
             int rnd = (int) (Math.random() * 52);
-            Card cardToMove = deck.get(i);
-            deck.set(i, deck.get(rnd));
-            deck.set(rnd, cardToMove);
+            Card cardToMove = deck[i];
+            deck[i] = deck[rnd];
+            deck[rnd] = cardToMove;
 
         }
     }
