cb99bd71333ddd399dec584c0a95e6c5c0738f41
veetip <veeti.pere@metropolia.fi>
2021-02-03 16:35:11 +0200

    Dealer class cleanup deleted useless code and added comments

--- src/main/java/model/Dealer.java
+++ src/main/java/model/Dealer.java
@@ -1,4 +1,3 @@
-
 package model;
 
 import java.io.Serializable;
@@ -11,7 +10,10 @@ public class Dealer implements Serializable {
         this.hand = new Hand();
     }
 
-    // Determines if dealer has a blackjack
+    /**
+     * Determines if dealer has a blackjack
+     */
+
     public boolean isBlackjack() {
         if (hand.calculateTotal() == 21) {
             return true;
@@ -19,21 +21,6 @@ public class Dealer implements Serializable {
             return false;
         }
     }
-
-    // This automates the dealer's play
-    /*public void dealerPlay(Deck deck) {
-        System.out.println("Dealer plays\n");
-        while (hand.calculateTotal() <= 16) {
-            System.out.println("Dealer has " + hand.calculateTotal() + " and hits");
-            hand.addCard(deck.nextCard());
-            //System.out.println("Dealer " + this.getHandString(true, false));
-        }
-        if (hand.calculateTotal() > 21) {
-            System.out.println("Dealer busts. ");// + this.getHandString(true, false));
-        } else {
-            System.out.println("Dealer stands at " + hand.calculateTotal());// + this.getHandString(true, false));
-        }
-    }*/
 
     /**
      * Prints Dealers hand
@@ -43,30 +30,30 @@ public class Dealer implements Serializable {
         hand.printHand();
     }
 
-    // Adds a card o the dealer's hand
+    /**
+     * Adds a card o the dealer's hand
+     */
     public void addCard(Card card) {
         hand.addCard(card);
     }
-    /*
-    // Gets the dealer's hand as a string
-    public String getHandString(boolean isDealer, boolean hideHoleCard) {
-        String str = "Cards:" + hand.toString(isDealer, hideHoleCard);
 
-        return str;
-    }
-    */
-
-    // Calculates the dealer's hand total
+    /**
+     * Calculates the dealer's hand total
+     */
     public int calculateTotal() {
         return hand.calculateTotal();
     }
 
-    // Clears the dealer's hand
+    /**
+     * Clears the dealer's hand
+     */
     public void clearHand() {
         hand.clearHand();
     }
 
-    // Peeks the dealer's face-down card
+    /**
+     * Peeks the dealer's face-down card
+     */
     public boolean peek() {
         return hand.dealerPeek();
     }
@@ -75,5 +62,4 @@ public class Dealer implements Serializable {
         return this.hand;
     }
 
-
-} //End class
+}
