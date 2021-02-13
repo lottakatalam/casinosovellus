64864821dd2c304bcf63d4840f7787b832d0ad46
teemuluh <teemu.luhtanen@metropolia.fi>
2021-02-13 21:02:07 +0200

    Added tableView to game history. Added History.java, which later tracks the round number, round result, bet and new balance.

--- src/main/java/view/GameHistoryController.java
+++ src/main/java/view/GameHistoryController.java
@@ -1,14 +1,20 @@
 package view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.History;
 
 import java.io.IOException;
 
@@ -19,6 +25,12 @@ public class GameHistoryController {
     public Button noButton;
     public ImageView blackScreen;
     public Button clearHistoryButton;
+    public TableView<History> historyTable;
+    public TableColumn gameColumn;
+    public TableColumn resultColumn;
+    public TableColumn betColumn;
+    public TableColumn balanceColumn;
+    public Button refreshButton;
 
     /**
      * Loads back to MainMenu.fxml
@@ -64,5 +76,24 @@ public class GameHistoryController {
         yesButton.setVisible(false);
         noButton.setVisible(false);
         areYouSure.setVisible(false);
+        historyTable.getItems().clear();
+    }
+
+    public ObservableList<History> getHistory() {
+        ObservableList<History> history = FXCollections.observableArrayList();
+        history.add(new History(1,"Won",100,2600));
+        history.add(new History(2,"Lost",100,2500));
+        history.add(new History(3,"Doubled",100,2900));
+        history.add(new History(4,"Insured",200,2900));
+        return history;
+    }
+
+    public void refresh() {
+        gameColumn.setCellValueFactory(new PropertyValueFactory<>("gameNumber"));
+        resultColumn.setCellValueFactory(new PropertyValueFactory<>("result"));
+        betColumn.setCellValueFactory(new PropertyValueFactory<>("bet"));
+        balanceColumn.setCellValueFactory(new PropertyValueFactory<>("balance"));
+        historyTable.setItems(getHistory());
+
     }
 }
