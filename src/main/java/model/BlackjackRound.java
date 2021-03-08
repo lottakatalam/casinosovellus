81e2bcd3ebcdbb59dfdc76781787ffda75cd9471
samueai <samuel.aitamaa@metropolia.fi>
2021-03-08 12:48:49 +0200

    splitPossibility

--- src/main/java/model/BlackjackRound.java
+++ src/main/java/model/BlackjackRound.java
@@ -74,10 +74,12 @@ public class BlackjackRound extends Thread {
         splitPossibility = checkSplitPossibility();
 
         /* THIS CAN BE USED TO DEBUG SPLITTING */
-        this.gameController.setSplitPossibility(true);
-        //this.gameController.setSplitPossibility(splitPossibility);
+        //this.gameController.setSplitPossibility(true);
+        this.gameController.setSplitPossibility(splitPossibility);
 
+        /* THIS CAN BE USED TO DEBUG DOUBLING */
         this.gameController.setDoublePossibility(doublePossibility);
+        //this.gameController.setDoublePossibility(true);
     }
 
     /**
