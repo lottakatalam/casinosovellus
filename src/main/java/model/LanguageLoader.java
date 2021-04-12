ec14f6a921a6df6ea9be5482fc5ce99757993a8f
ilkkajs <ilkka.savela@metropolia.fi>
2021-04-12 11:22:32 +0300

    - fix #7 for jenkins problem

--- src/main/java/model/LanguageLoader.java
+++ src/main/java/model/LanguageLoader.java
@@ -47,7 +47,7 @@ public class LanguageLoader {
         textResources = new Properties();
         String TRFile = String.format("TextResources_%s_%s.properties", locale.getLanguage(),locale.getCountry());
         try {
-            textResources.load(LanguageLoader.class.getClassLoader().getResourceAsStream(TRFile));
+            textResources.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(TRFile));
         } catch (IOException e) {
             e.printStackTrace();
         }
